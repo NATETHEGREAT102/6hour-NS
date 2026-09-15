a7893f382cefb261fb44d5e70868d9a7cc6e5133
Nate shaw <ns129012@student.wpsok.org>
2026-09-15 13:41:11 -0500

    ture in

---
+++ hw5.py
@@ -0,0 +1,26 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW5
+
+#1. Print Hello World!
+print("Hello World")
+#1. Create a list with 5 strings containing 5 different names in it.
+liststring=["bob","bobby","bobert","bob JR","jeff"]
+#2. Append a new name onto the Name List.
+liststring.append("bob SR")
+#3. Print out the 4th name on the list.
+print(liststring[3])
+#4. Create a list with 4 different integers in it.
+intlist=[1,2,3,4]
+#5. Insert a new integer into the 2nd spot and print the new list.
+intlist.insert(1,3)
+print(intlist)
+#6. Sort the list from lowest to highest and print the sorted list.
+intlist.sort()
+print(intlist)
+#7. Add the 1st three numbers on the sorted list together and print the sum.
+listsum=intlist[0]+intlist[1]+intlist[2]
+print(listsum)
+#8. Create a list with two strings, two variables, and two boolean values.
+mixlist=["bob","boby",2,4,True,False]
+#9. Create a print statement that asks the user to input their own index value for the list on #8.
---
+++ hw6.py
@@ -0,0 +1,23 @@
+#Name:nate
+#Class: 6th Hour
+#Assignment: HW6
+
+
+#1. Create a list with 9 different numbers inside.
+numlist=[1,2,3,4,5,6,7,8,9]
+print(numlist)
+#2. Sort the list from highest to lowest.
+numlist.sort(reverse=True)
+#3. Create an empty list.
+emplist=[]
+#4. Remove the median number from the first list and add it to the second list.
+numlist.pop(4)
+#5. Remove the first number from the first list and add it to the second list.
+
+#6. Print both lists.
+
+#7. Add the two numbers in the second list together and print the result.
+
+#8. Add the sum from #7 to the first list.
+
+#9. Sort the first list from lowest to highest and print it.
